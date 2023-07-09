fec71362bf52361ed51300698daba0d539978b99
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-07-10 00:01:56 +0500

    modified:   Notes/React/3. Baby Steps.js

--- Notes/React/3. Baby Steps.js
+++ Notes/React/3. Baby Steps.js
@@ -9,13 +9,14 @@ Complete the getReactVersion method such that it returns the current installed v
 /**
  * @param {number[]} numbers
  */
-function getPositiveNumbers(numbers) {
-    return numbers.filter(number => number > 0);
+import React from "react";
+
+function getReactVersion() {
+    return React.version;
 }
 
 // sample usage (do not modify)
-console.log(getPositiveNumbers([10, -5, 2, -4]));
-
+console.log(getReactVersion());
 
 
 /************************SUBMITTED************************/
