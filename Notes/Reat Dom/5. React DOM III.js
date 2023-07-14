46edd810bdfca9e561b0c7faa7bef8c6737bac99
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-07-14 23:01:50 +0500

    new file:   Notes/Reat Dom/5. React DOM III.js

---
+++ Notes/Reat Dom/5. React DOM III.js
@@ -0,0 +1,33 @@
+/* 5. React DOM II *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+Render an <h1>Online Supermarket</h1> on the page inside the given root.
+
+*/
+
+/********************************/
+/*REACT DOM : INDEX.JS*/
+/********************************/
+import React from "react";
+import {createRoot} from "react-dom/client";
+
+const element2 = React.createElement("h1", {}, "Online Supermarket");
+const root2 = document.querySelector("#react-root");
+
+createRoot(root2).render(element2);
+
+
+
+/************************SUBMITTED************************/
+import React from "react";
+import {createRoot} from "react-dom/client";
+
+const element = React.createElement("h1", {}, "Online Supermarket");
+const root = document.querySelector("#react-root");
+
+createRoot(root).render(element);
+/***************************************************************************************/
+/***************************************************************************************/
+/********************************/
+/*REACT DOM : INDEX.HTML*/
+/********************************/
+<div id="react-root"></div>
+/***************************************************************************************/
