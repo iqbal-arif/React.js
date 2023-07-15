7123cd6ddfcf3abbad295d53e47e06b3b90ad3d6
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-07-15 19:57:55 +0500

    new file:   Notes/Intro to JSX/6. Supermarket tagline.js

---
+++ Notes/Intro to JSX/6. Supermarket tagline.js
@@ -0,0 +1,28 @@
+/* 6. Supermarket tagline -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+Using JSX, complete the getTagline method such that it returns an h2 element with the class: subtitle and the text: Grocery shopping simplified.
+*/
+
+/********************************/
+/* ATTRIBUTES I : INDEX.JS*/
+/********************************/
+function getTagline() {
+    return <h2 className="subtitle">Grocery shopping simplified</h2>;
+}
+
+// sample usage (do not modify)
+console.log(getTagline());
+
+/************************SUBMITTED************************/
+function getTagline() {
+    return <h2 className="subtitle">Grocery shopping simplified</h2>
+   }
+   
+   // sample usage (do not modify)
+   console.log(getTagline());
+/***************************************************************************************/
+
+/********************************/
+/* ATTRIBUTES I : INDEX.HTML*/
+/********************************/
+{/* <div id="react-root"></div> */}
+/***************************************************************************************/*/
