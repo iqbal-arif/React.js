56b6da30719eef1351db44a551a6b2c7c020a6fc
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-07-16 19:02:21 +0500

    new file:   Notes/JSX Expressions/2. Navbar.js

---
+++ Notes/JSX Expressions/2. Navbar.js
@@ -0,0 +1,41 @@
+/* 2. Navbar -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+Complete the getNavbar function such that it returns a div with the message: Logged in as USER. Log out
+Where USER is the name of the user received as an argument.
+
+*/
+
+/********************************/
+/* INTRO JSX : INDEX.JS*/
+/********************************/
+function getNavbar(user) {
+    return <div>Logged in as {user.name}. Log out</div>;
+}
+
+// sample usage (do not modify)
+const element2 = getNavbar({
+    name: "Sam",
+    age: 34
+});
+console.log(element2);
+
+/************************SUBMITTED************************/
+function getNavbar(user) {
+    if (user){
+        return <div>Logged in as {user.name}. log out</div>
+    } 
+}
+
+// sample usage (do not modify)
+const element = getNavbar({
+    name: "Sam",
+    age: 34
+});
+console.log(element);
+/***************************************************************************************/
+
+/********************************/
+/* INTRO JSX : INDEX.HTML*/
+/********************************/
+{/* <div id="react-root"></div> */}
+/***************************************************************************************/*/
