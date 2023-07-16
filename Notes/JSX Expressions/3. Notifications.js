894edabaebcadc91db6c1a1db819300c2cebb28a
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-07-16 20:02:02 +0500

    modified:   Notes/JSX Expressions/3. Notifications.js

--- Notes/JSX Expressions/3. Notifications.js
+++ Notes/JSX Expressions/3. Notifications.js
@@ -8,16 +8,21 @@ Where X is the number of notifications.
 /********************************/
 /* INTRO JSX : INDEX.JS*/
 /********************************/
-function getNavbar(user) {
-    return <div>Logged in as {user.name}. Log out</div>;
+function getNotifications(notifications) {
+    return <p>You have {notifications.length} new notifications</p>
 }
 
 // sample usage (do not modify)
-const element2 = getNavbar({
-    name: "Sam",
-    age: 34
-});
-console.log(element2);
+const notifications = [{
+    id: 1,
+    text: "Your order has been delivered"
+}, {
+    id: 2,
+    text: "Your coupon code has expired"
+}];
+const element = getNotifications(notifications);
+console.log(element);
+
 
 /************************SUBMITTED************************/
 function getNotifications(notifications) {
