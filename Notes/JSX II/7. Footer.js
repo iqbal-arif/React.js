acb0e2ab58776dc2bc6855507868c1cb56094add
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-07-18 12:22:55 +0500

    new file:   Notes/JSX II/7. Footer.js

---
+++ Notes/JSX II/7. Footer.js
@@ -0,0 +1,57 @@
+/* 3. Footer *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+Complete the getFooter function such that it returns the following HTML structure:
+
+  <h3>Your online supermarket</h3>
+  <ul>
+      <li>Contact</li>
+      <li>Press</li>
+      <li>About</li>
+  </ul>
+  <p>All rights reserved &copy;</p>
+
+*/
+
+/********************************/
+/* JSX II : INDEX.JS*/
+/********************************/
+function getFooter() {
+    return (
+        <>
+            <h3>Your online supermarket</h3>
+            <ul>
+                <li>Contact</li>
+                <li>Press</li>
+                <li>About</li>
+            </ul>
+            <p>All rights reserved &copy;</p>
+        </>
+    );
+}
+
+// sample usage (do not modify)
+console.log(getFooter());
+
+
+/************************SUBMITTED************************/
+function getFooter() {
+    return(<>
+      <h3>Your online supermarket</h3>
+      <ul>
+          <li>Contact</li>
+          <li>Press</li>
+          <li>About</li>
+      </ul>
+      <p>All rights reserved &copy;</p>
+    </>)
+    }
+    
+    // sample usage (do not modify)
+    console.log(getFooter());
+/***************************************************************************************/
+
+/********************************/
+/* JSX II : INDEX.HTML*/
+/********************************/
+{/* <div id="react-root"></div> */}
+/***************************************************************************************/*/
