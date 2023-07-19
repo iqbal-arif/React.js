1c9c297910f166b6c20379f4d04d2a0c16ff9e5b
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-07-20 00:00:05 +0500

    new file:   Notes/Components/6. One Component per file.js

---
+++ Notes/Components/6. One Component per file.js
@@ -0,0 +1,8 @@
+One Component per file
+
+Create the Hero Component in Hero.js that returns the following HTML:
+
+<h1 class="title">Supermarket</h1>
+<p>Start shopping</p>
+
+and then render it in the DOM from the index.js.
