de623902aedf83574fb266570a61d4c0c5315d69
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-07-20 20:33:19 +0500

    new file:   Notes/Components/7. Multiple Components.js

---
+++ Notes/Components/7. Multiple Components.js
@@ -0,0 +1,83 @@
+ 
+/* 7. Multiple Components *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+    Create a Button Component in Button.js that returns an button element with the text: Button text
+    Create a Link Component in Link.js that returns an a element with the text: Link text
+    From the index.js using the 2 components above, render 2 consecutive buttons followed by 1 link inside the App Component.
+
+Note: Notice how the App is the main Component being rendered and this Component renders other ones. This is typical in most React apps where there's only 1 render call. You should not call render more than once.
+*/
+
+/********************************/
+/* COMPONENT : INDEX.JS*/
+/********************************/
+import {createRoot} from "react-dom/client";
+import Button from "./Button.js";
+import Link from "./Link.js";
+
+function App() {
+    return (
+        <>
+            <Button />
+            <Button />
+            <Link />
+        </>
+    );
+}
+
+// Sample usage (do not modify)
+createRoot(document.querySelector("#react-root")).render(<App />);
+
+
+/************************SUBMITTED************************/
+import {createRoot} from "react-dom/client";
+import Button from "./Button.js";
+import Link from "./Link.js";
+
+
+function App() {
+    return (<>
+         <Button />
+         <Button />
+         <Link />
+
+    </>);
+}
+
+const root = document.querySelector("#react-root");
+
+createRoot(root).render(<App />);
+
+// Sample usage (do not modify)
+createRoot(document.querySelector("#react-root")).render(<App />);
+
+/********************************/
+/* COMPONENT : HERO.JS*/
+/********************************/
+// Define the Hero Component
+export default function Hero(){
+    return (
+        <>
+            <h1 className="title">Supermarket</h1>
+            <p>Start shopping</p>
+        </>
+    )
+}
+
+/************************SUBMITTED************************/
+// Define the Hero Component
+export default function Hero(){
+    return(
+        <>
+        <h1 className="title">Supermarket</h1>
+        <p>Start shopping</p>
+        </>
+    );
+}
+/***************************************************************************************/
+
+/********************************/
+/* COMPONENT : INDEX.HTML*/
+/********************************/
+{ <div id="react-root"></div>}
+/***************************************************************************************/
