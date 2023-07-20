eb38a69bea5476f89be01a5281f70f7b364343a4
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-07-20 15:08:47 +0500

    modified:   Notes/Components/6. One Component per file.js

--- Notes/Components/6. One Component per file.js
+++ Notes/Components/6. One Component per file.js
@@ -1,4 +1,4 @@
-One Component per file
+/* 4. One Component per file *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
 Create the Hero Component in Hero.js that returns the following HTML:
 
@@ -6,3 +6,55 @@ Create the Hero Component in Hero.js that returns the following HTML:
 <p>Start shopping</p>
 
 and then render it in the DOM from the index.js.
+*/
+
+/********************************/
+/* COMPONENT : INDEX.JS*/
+/********************************/
+import {createRoot} from "react-dom/client";
+
+import Hero from "./Hero";
+
+const root = document.querySelector("#react-root");
+createRoot(root).render(<Hero />);
+
+/************************SUBMITTED************************/
+import {createRoot} from "react-dom/client";
+
+import Hero from "./Hero.js";
+
+const root = document.querySelector("#react-root");
+createRoot(root).render(<Hero/>);
+
+// TODO:
+
+/********************************/
+/* COMPONENT : HERO.JS*/
+/********************************/
+// Define the Hero Component
+export default function Hero(){
+    return (
+        <>
+            <h1 className="title">Supermarket</h1>
+            <p>Start shopping</p>
+        </>
+    )
+}
+
+/************************SUBMITTED************************/
+// Define the Hero Component
+export default function Hero(){
+    return(
+        <>
+        <h1 className="title">Supermarket</h1>
+        <p>Start shopping</p>
+        </>
+    );
+}
+/***************************************************************************************/
+
+/********************************/
+/* COMPONENT : INDEX.HTML*/
+/********************************/
+{ <div id="react-root"></div>}
+/***************************************************************************************/
