38e66f55195cb18a0c2ff6c181dd108480c9047b
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-07-22 00:04:53 +0500

    modified:   Notes/8. Props/6. Button II.js

--- Notes/8. Props/6. Button II.js
+++ Notes/8. Props/6. Button II.js
@@ -10,39 +10,18 @@ Complete the Button component such that it's able to render a custom text, for e
 import {createRoot} from "react-dom/client";
 
 function Button(props) {
-    const size = props.size;
-    let className = "btn-medium";
-
-    if (size === "small"){
-        className = "btn-small";
-    } else if (size === "large"){
-        className = "btn-large";
-    }
-
-    return <button className={className}>Text here</button>;
+    return <button>{props.children}</button>;
 }
 
 const root = document.querySelector("#react-root");
-createRoot(root).render(<Button size="small" />);
-/***********************************************************/
-import {createRoot} from "react-dom/client";
 
-function Button(props) {
-    const size = props.size ?? "medium";
-
-    return <button className={`btn-${size}`}>Text here</button>;
-}
-
-const root = document.querySelector("#react-root");
-createRoot(root).render(<Button size="small" />);
-
-
+createRoot(root).render(<Button>Login</Button>);
 
 /************************SUBMITTED************************/
 import {createRoot} from "react-dom/client";
 
-function Button(){
-    return null;
+function Button(props){
+    return <button>{props.children}</button>;
 }
 
 const root = document.querySelector("#react-root");
