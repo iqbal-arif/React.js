87408381eea4893a4a3bcfe410b0e1b19aa7828d
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-07-21 08:05:41 +0500

    modified:   Notes/8. Props/3. More Props.js

--- Notes/8. Props/3. More Props.js
+++ Notes/8. Props/3. More Props.js
@@ -14,12 +14,28 @@ Fix the Navbar component such that it renders:
 /********************************/
 import {createRoot} from "react-dom/client";
 
-function ShoppingCart(props) {
-    return <div>{props.count} items in your cart</div>;
+function Navbar(props) {
+    return (<>
+        <div>Welcome {props.user.first_name} {props.user.last_name}</div>
+        <p>You've got {props.notifications.length} notifications</p>
+    </>);
 }
 
+let notifications=[{
+    id: 1,
+    text: "Order delivered"
+}, {
+    id: 2,
+    text: "Order received"
+}];
+
+const user = {
+    first_name: "Sam",
+    last_name: "Wolf"
+};
+
 const root = document.querySelector("#react-root");
-createRoot(root).render(<ShoppingCart count="5" />);
+createRoot(root).render(<Navbar notifications={notifications} user={user} />);
 
 
 /************************SUBMITTED************************/
