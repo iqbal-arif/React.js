9209fb3312dd8495ba50ad68eb925e16dbf59364
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-07-26 01:23:03 +0500

    modified:   Project/1. Project I/input.js

--- Project/1. Project I/input.js
+++ Project/1. Project I/input.js
@@ -8,3 +8,12 @@ export default function Input(props){
            <div className="ui-textfield" placeholder={props.placeholder} name={props.name} type={type}>{props.children}</div>
            )
   }
+
+/*************Solution******************/
+
+  export default function Input(props) {
+    // destructuring with default value
+    // https://codetogo.io/how-to-destructure-with-default-value-in-javascript/
+    const {type = "text", placeholder, name} = props;
+    return <input className="ui-textfield" type={type} placeholder={placeholder} name={name} />;
+}
