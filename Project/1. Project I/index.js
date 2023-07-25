a4cdd2910f30faede297722e460aa33ae25c7daa
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-07-26 01:24:30 +0500

    modified:   Project/1. Project I/index.js

--- Project/1. Project I/index.js
+++ Project/1. Project I/index.js
@@ -2,6 +2,7 @@ import {createRoot} from "react-dom/client";
 import Link from "./Link.js";
 import Button from "./Button.js";
 import Container from "./Container.js"
+import Input from "./Input.js"
 
 function App() {
     return (
@@ -9,8 +10,31 @@ function App() {
         <Link/>
         <Button/>
         <Container/>
+        <Input/>
         </>
     );
+}
+
+createRoot(document.querySelector("#react-root")).render(<App />);
+
+
+/*************Solution******************/
+
+import {createRoot} from "react-dom/client";
+import Link from "./Link.js";
+import Container from "./Container.js";
+import Button from "./Button.js";
+import Input from "./Input.js";
+
+function App() {
+    return (<>
+        <Container>
+            { /* It doesn't matter what you write here. This is where you can use the components that you've defined. It's important, however, to import them in this file. */ }
+            <Link href="https://react-tutorial.app">React Tutorial</Link>
+            <Button disabled={true}>Login</Button>
+            <Input placeholder="Full Name" name="full_name" />
+        </Container>
+    </>);
 }
 
 createRoot(document.querySelector("#react-root")).render(<App />);
