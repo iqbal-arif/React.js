6f51e06506d82aa4c424f280d977c55000ba718c
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-07-26 01:20:42 +0500

    modified:   Project/1. Project I/Button.js

--- Project/1. Project I/Button.js
+++ Project/1. Project I/Button.js
@@ -3,3 +3,10 @@ export default function Button(props){
    return(
        <div className="ui-button" type={props.type} disabled={props.disabled} >{props.children}</div>) 
 } 
+
+/*************Solution******************/
+
+export default function Button(props) {
+    const {children, type, disabled} = props;
+    return <button className="ui-button" type={type} disabled={disabled}>{children}</button>;
+}
