b88928321e63b0822d5281aba84bcd450bc6e9ec
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-07-26 23:42:31 +0500

    modified:   Notes/10. Pure Functions/2. Impure to Pure.js

--- Notes/10. Pure Functions/2. Impure to Pure.js
+++ Notes/10. Pure Functions/2. Impure to Pure.js
@@ -8,31 +8,54 @@ It should not modify its props and it should produce the same output when given 
 /********************************/
 import {createRoot} from "react-dom/client";
 
-function Hero(){
-    return (
-      <>
-        <h1>Welcome to our supermarket</h1>
-        <p>Start shopping</p>
-      </>
-    );
+function Navbar(props) {
+    const full_name = `${props.user.first_name} ${props.user.last_name}`;
+    return (<div>
+        Hello {full_name}.<br />
+        You are now {props.user.age + 1} years old.
+    </div>);
 }
 
-// do not modify
-createRoot(document.querySelector("#react-root")).render(<Hero></Hero>);
+const user = {
+    first_name: "Sam",
+    last_name: "Doe",
+    age: 26
+}
+
+const root = document.querySelector("#react-root");
+// does the Component render the same thing
+// when given the same props?
+createRoot(root).render(<>
+    <Navbar user={user} />
+    <Navbar user={user} />
+</>);
+
 
 /************************SUBMITTED************************/
 import {createRoot} from "react-dom/client";
- function Hero(){
-     return (
-         <div>
-             <h1>Welcome to our supermarket</h1>
-            <p>Start shopping</p>
-         </div>
-     );
- }
 
-// do not modify
-createRoot(document.querySelector("#react-root")).render(<Hero></Hero>);
+function Navbar(props) {
+    const full_name = `${props.user.first_name} ${props.user.last_name}`;
+    const age = props.user.age;
+    return (<div>
+        Hello {full_name}.<br />
+        You are now {age+1} years old.
+    </div>);
+}
+
+const user = {
+    first_name: "Sam",
+    last_name: "Doe",
+    age: 26
+}
+
+const root = document.querySelector("#react-root");
+// does the Component render the same thing
+// when given the same props?
+createRoot(root).render(<>
+    <Navbar user={user} />
+    <Navbar user={user} />
+</>);
 /***************************************************************************************/
 
 /********************************/
