6de8a44469beaf32588f2c0c7b327357b86c9624
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-09-26 12:29:38 -0700

    Update input.js

--- Project/2. Project II/input.js
+++ Project/2. Project II/input.js
@@ -1,8 +1,11 @@
+import clsx from "clsx"
 export default function Input(props) {
     let type = props.type;
     if (!type){
         type = "text";
     }
-
-    return <input className="ui-textfield" type={type} placeholder={props.placeholder} name={props.name} />;
+    console.log(props)
+    const {name, placeholder,tabIndex, value} = props;
+    const inputClasses = clsx(props.className, "ui-textfield")
+    return <input className={inputClasses} type={type} tabIndex={tabIndex} placeholder={props.placeholder} name={props.name} value={value}/>;
 }
