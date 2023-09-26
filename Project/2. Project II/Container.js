44779f17dfb87257d38d8e93d75bde3071912368
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-09-26 12:34:49 -0700

    Update Container.js

--- Project/2. Project II/Container.js
+++ Project/2. Project II/Container.js
@@ -1,3 +1,20 @@
+*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*
+******************SOLUTION******************
+*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*
+import clsx from "clsx";
+
+export default function Container(props) {
+    const {children, className, ...rest} = props;
+    const classes = clsx("ui-container", className);
+
+    return <div className={classes} {...rest}>{children}</div>;
+}
+
+*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*
+******************SUBMITTED******************
+*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*
+
+
 import clsx from "clsx";
 export default function Container(props) {
     const {children,tabIndex} = props;
