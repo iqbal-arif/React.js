875ece16990e710ab30ad51fc64f89d066435b63
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-09-26 11:52:49 -0700

    Update link.js

--- Project/2. Project II/link.js
+++ Project/2. Project II/link.js
@@ -1,74 +1,9 @@
 import clsx from "clsx";
 
-export default function Link(props,classes) {
-    console.log(props)
-    let linkClass=classes;
-    const className = clsx("ui-link",linkClass);
-    console.log(className)
-    const {children, ...rest} = props;
-
-    return <a className={className} {...rest}>{children}</a>;
-}
-
-import clsx from "clsx";
-
-export default function Link(props,classes) {
-    console.log(props)
-    let linkClass=classes;
-    const className = clsx("ui-link");
-    console.log(className)
-    const {children, ...rest} = props;
-
-    return <a className={className} {...rest}>{children}</a>;
-}
-
-
 export default function Link(props) {
-    console.log(props)
-    // let linkClass=classes;
-    // console.log (linkClass)
-    const className = "ui-link";
-    // console.log(className)
-    const {children, ...rest} = props;
-
-    return <a className={className} {...rest}>{children}</a>;
+    const {children, href, tabIndex} = props;
+    const linkClasses = clsx(props.className, "ui-link")
+    // console.log({disabled})
+    return <a className={linkClasses} tabIndex={props.tabIndex}  href={href}>{children}</a>;
 }
-
-import clsx from "clsx";
-
-const result = clsx({
-    "link": true,
-    "link-primary": false
-});
-
-console.log(result); //"link"
-
-/*
-const className = clsx({
-        "text": true, // this class is always added
-        "title": props.loggedIn
-    });
-*/
-
-import clsx from "clsx";
-export default function Link(props,className) {
-    console.log(props)
-    const className= clsx({"ui-link":true,class1:{className}})
-    // const className = "ui-link";
-    const {children,...rest} = props;
-    // const {className="ui-link",...rest} = props;
-
-    console.log (<a className={className} {...rest}>{children}</a>);
-    return (<a className={className} {...rest}>{children}</a>);
-
-}
-
-import clsx from "clsx"
-export default function Link(props) {
-    const {children, href, disabled} = props;
-    console.log(props)
-    let dynamicClass = children
-    const classes = clsx(dynamicClass,"ui-link")
-    console.log(classes)
-    return <a className={classes} href={href}>{children} disabled={disabled}</a>;
 }
