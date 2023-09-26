7f173100c20fee1efa23c2ea32fba192728db69e
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-09-26 12:29:06 -0700

    Update index.js

--- Project/2. Project II/index.js
+++ Project/2. Project II/index.js
@@ -9,7 +9,7 @@ function App() {
         <Container className="app" tabIndex={2}>
             <Link className = "link-kit" tabIndex={3} href="https://react-tutorial.app">React Tutorial</Link>
             <Button className = "link-button" tabIndex={3} disabled={true}>Login</Button>
-            <Input className="link-Input" placeholder="Full Name" name="full_name" />
+            <Input className="link-Input" tabIndex={5} placeholder="Full Name" name="full_name" defaultValue= "default val"/>
         </Container>
     </>);
 }
