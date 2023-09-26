148cdd93a5384f4a851bbc69782abcdc9c67fa2c
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-09-26 12:33:11 -0700

    Update link.js

--- Project/2. Project II/link.js
+++ Project/2. Project II/link.js
@@ -1,4 +1,19 @@
+*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*
+******************SOLUTION******************
+*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*
+import clsx from "clsx";
 
+export default function Link(props) {
+    const {children, className, ...rest} = props;
+    const classes = clsx("ui-link", className);
+
+    return <a className={classes} {...rest}>{children}</a>;
+}
+
+
+*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*
+******************SUBMITTED******************
+*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*
 import clsx from "clsx";
 export default function Container(props) {
     const {children,tabIndex} = props;
