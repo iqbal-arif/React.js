7e106e52f6041dfbb1056751674ca761dfda098e
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-09-27 10:46:48 -0700

    Create 3. Array Destructure II.js

---
+++ Notes/13. Array Destructuring/3. Array Destructure II.js
@@ -0,0 +1,5 @@
+Array destructuring II
+
+Do not modify any of the code in this challenge, but simply add 1 line under the // TODO where you destructure the user array into name and age variables.
+
+We will test that you have used the destructuring syntax for this challenge.
