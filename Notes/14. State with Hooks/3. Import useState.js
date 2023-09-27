64cfefc2d29076c510b40dae1d344d9b0482bbb8
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-09-27 12:00:04 -0700

    Update 3. Import useState.js

--- Notes/14. State with Hooks/3. Import useState.js
+++ Notes/14. State with Hooks/3. Import useState.js
@@ -1,3 +1,51 @@
 Import useState
 
 This is a basic challenge. You only have to import the useState function.
+
+/* 3. Dark theme I *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+Update the Navbar Component such that it renders:
+
+    in dark mode when the theme is "dark"
+    in light mode when the theme is "light"
+
+We've already given you the styles for dark theme and light theme which you need to apply.
+
+*/
+
+/********************************/
+/*  JSX ADVANCED : INDEX.JS*/
+/********************************/
+import {createRoot} from "react-dom/client";
+
+function Navbar(props) {
+    return <h1 className={props.theme}>Online supermarket</h1>
+}
+
+const root = document.querySelector("#react-root");
+
+createRoot(root).render(<>
+        <Navbar theme="light" />
+        <Navbar theme="dark" />
+    </>);
+
+
+
+/************************SUBMITTED************************/
+import {createRoot} from "react-dom/client";
+
+function Navbar(props) {
+    // console.log(props.theme)
+ if (props.theme!=="light"){
+    return <h1 className="dark">Online supermarket</h1>
+}else{
+    return <h1 className="light">Online supermarket</h1>
+}
+}
+const root = document.querySelector("#react-root");
+
+createRoot(root).render(<>
+        <Navbar theme="light" />
+        <Navbar theme="dark" />
+    </>);
+/***************************************************************************************/
