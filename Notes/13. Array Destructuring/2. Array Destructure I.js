307ca905615d5ece7163c4065c6be890930ef768
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-09-27 10:44:19 -0700

    Create 2. Array Destructure I.js

---
+++ Notes/13. Array Destructuring/2. Array Destructure I.js
@@ -0,0 +1,46 @@
+/* 2. Array destructuring I *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+Do not modify any of the code in this challenge, but simply add 1 line under the // TODO where you destructure the name array into first and last variables.
+
+We will test that you have used the destructuring syntax for this challenge.
+
+*/
+
+/********************************/
+/*  ARRAY DESTRUCTURE : INDEX.JS*/
+/********************************/
+/**
+ * @param {string[]} name
+ */
+function printFullName(name) {
+    console.log(name);
+    const [first, last] = name;
+    return `${first} ${last}`;
+}
+
+
+// Sample usage (do not modify)
+console.log(printFullName(["Sam", "Doe"]));
+console.log(printFullName(["John", "Malaga"]));
+
+/************************SUBMITTED************************/
+/**
+ * @param {string[]} name
+ */
+function printFullName(name) {
+    console.log(name);
+    // TODO: write your code below this line
+    const [first, last] = name;
+    return `${first} ${last}`;
+}
+
+
+// Sample usage (do not modify)
+console.log(printFullName(["Sam", "Doe"]));
+console.log(printFullName(["John", "Malaga"]));
+/***************************************************************************************/
+
+/********************************/
+/* PURE FUNCTIONS : INDEX.HTML*/
+/********************************/
+<div id="react-root"></div>
+/***************************************************************************************/*/
