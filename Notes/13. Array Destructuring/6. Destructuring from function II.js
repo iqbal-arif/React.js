b2ca46c02a258bee75ce593372ae9698c53ba26f
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-09-27 11:16:29 -0700

    Update 6. Destructuring from function II.js

--- Notes/13. Array Destructuring/6. Destructuring from function II.js
+++ Notes/13. Array Destructuring/6. Destructuring from function II.js
@@ -1,3 +1,44 @@
-Destructuring from functions II
-
+/* 6. Destructuring from functions IIs *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 Complete the getPrice function in helpers.js such that it returns the price number and the describePrice function.
+*/
+
+/********************************/
+/*  ARRAY DESTRUCTURE : HELPERS.JS*/
+/********************************/
+//helpers.js
+export default function getPrice() {
+    const price = 10;
+
+    function describePrice() {
+        return `The price is ${price} euros`;
+    }
+
+    return [price, describePrice];
+}
+/************************SUBMITTED************************/
+export default function getPrice() {
+    const price = 10;
+
+    function describePrice() {
+        return `The price is ${price} euros`;
+    }
+
+    // TODO: return both the price and describePrice
+    return [price, describePrice] 
+}
+
+/***************************************************************************************/
+
+/********************************/
+/* ARRAY DESTRUCTURE : INDEX.JS*/
+/********************************/
+import getPrice from "./helpers.js";
+
+function destructureTest() {
+    const [price, describePrice] = getPrice();
+    console.log(price);
+    console.log(describePrice());
+}
+
+destructureTest();
+*************************************************************************************/*/
