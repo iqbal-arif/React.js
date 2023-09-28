06486b0cd9eabe97ebadc715bf84c1436d81eabc
ARIF IQBAL <59909963+iqbal-arif@users.noreply.github.com>
2023-09-28 09:11:08 -0700

    Update 5.  Counter.js

--- Notes/14. State with Hooks/5.  Counter.js
+++ Notes/14. State with Hooks/5.  Counter.js
@@ -1,27 +1,22 @@
-/* 5. Import useState *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-This is a basic challenge. You only have to import the useState function.
+/* 5. Counter *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+Complete the Counter Component such that it creates a state for the seconds elapsed.
+It should also return <h2>X seconds elapsed</h2> where X is the number of seconds elapsed.
+Note that you will have to destructure both state variables to pass the tests. This is to help you practice the syntax.
 */
 
 /********************************/
 /*  STATE W HOOKS : INDEX.JS*/
 /********************************/
-import {useState} from "react";
-import {createRoot} from "react-dom/client";
 
-function App() {
-    return <div>Hello World</div>;
-}
-
-createRoot(document.querySelector("#react-root")).render(<App />);
 
 /************************SUBMITTED************************/
 import {createRoot} from "react-dom/client";
-import {useState} from "react"
-function App() {
-    return <div>Hello World</div>;
+
+function Counter() {
+    return <h2></h2>;
 }
 
-createRoot(document.querySelector("#react-root")).render(<App />);
+createRoot(document.querySelector("#react-root")).render(<Counter />);
 /***************************************************************************************/
 /********************************/
 /*  STATE W HOOKS : INDEX.HTML*/
